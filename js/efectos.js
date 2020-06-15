f562b931c0b3a02a715fd1eb27c3817a91e66bd2
Daniel SA <d.santamaria@uniandes.edu.co>
2020-06-15 17:47:46 -0500

    eventos y contacto terminados

--- js/efectos.js
+++ js/efectos.js
@@ -1,4 +1,14 @@
+//Menú desplegable-------------------------------
 $(".menu-toggle-btn").click(function(){
     $(this).toggleClass("fa-times");
     $(".navigation-menu").toggleClass("active");
+});
+
+//Carrusel----------------------------------------
+$(".owl-carousel").owlCarousel({
+    items: 2,
+    margin:20,
+    loop: true,
+    center:true,
+    dots: false
 });
